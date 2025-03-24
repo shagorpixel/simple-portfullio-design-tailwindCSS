504e014eb2bace895edecd5c4394744e8c175431
yourusername <shagorpixel@gmail.com>
2025-03-24 17:00:32 +0600

    Add all Image For Profect

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {
+      fontFamily: {
+        primary : ["Montserrat", "sans-serif"],
+        // Add more custom font families as needed
+      },
+      colors:{
+        primary: '#FC3314',
+        secoundary:'#FFD4D0'
+      }
     }
   },
   plugins: [],
